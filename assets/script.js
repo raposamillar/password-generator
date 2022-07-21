e16815970f1b593280d07f60561c6452ef9735c4
raposamillar <lisa.raposamillar@gmail.com>
2022-07-21 15:38:47 -0400

    add initial prompt

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,9 @@
-// Assignment code here
 
+const handleClick = () => {
+  let len = prompt('How long would you like the password to be (8-128)?');
+  if(len<8 || len>128) return handleClick;
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+  
+};
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+document.querySelector('button').addEventListener('click', handleClick);
