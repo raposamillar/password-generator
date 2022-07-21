aad2cdfddecb6d9a0c2fb4ace2e8294f4cea90df
raposamillar <lisa.raposamillar@gmail.com>
2022-07-21 15:40:48 -0400

    add series of prompts for password criteria

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,13 @@
-
 const handleClick = () => {
   let len = prompt('How long would you like the password to be (8-128)?');
   if(len<8 || len>128) return handleClick;
+
+  let lower = confirm("Would you like your password to have lowercase letters?");
+  let upper = confirm("Would you like your password to have uppercase letters?");
+  let numeric = confirm("Would you like your password to have numeric values?");
+  let special = confirm("Would you like your password to have special characters?");
+
+  let temp = '';
 
   
 };
